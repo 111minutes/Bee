079da05161978d8bbd6a98f7ceebb0f90287e718
sergeyzenchenko <mail@sergey-zenchenko.com>
2012-02-25 17:32:01 +0200

    add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bee::Application.config.secret_token = 'a45fd53e0794c700de250fc449079300d90c85364ac9cbe238290b678ed1512161314ef700aa349ac473f4fec963e6060147f3d0396c3fe2eb5037d9ad25a6e1'
